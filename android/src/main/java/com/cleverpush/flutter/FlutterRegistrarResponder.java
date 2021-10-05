03ffa2d0790457e2889d81c016139ab079219a61
Hardik Lakum <lakum@peerbits.com>
2021-10-05 19:03:54 +0530

    hotfix/CP-2326 solved flutter android crash

--- android/src/main/java/com/cleverpush/flutter/FlutterRegistrarResponder.java
+++ android/src/main/java/com/cleverpush/flutter/FlutterRegistrarResponder.java
@@ -1,6 +1,8 @@
 package com.cleverpush.flutter;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Looper;
 
 import java.util.HashMap;
 
@@ -8,47 +10,52 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
 
 abstract class FlutterRegistrarResponder {
-   MethodChannel channel;
-   PluginRegistry.Registrar flutterRegistrar;
+    MethodChannel channel;
+    PluginRegistry.Registrar flutterRegistrar;
 
-   void replySuccess(final MethodChannel.Result reply, final Object response) {
-      runOnMainThread(new Runnable() {
-         @Override
-         public void run() {
-            reply.success(response);
-         }
-      });
-   }
+    void replySuccess(final MethodChannel.Result reply, final Object response) {
+        runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                reply.success(response);
+            }
+        });
+    }
 
-   void replyError(final MethodChannel.Result reply, final String tag, final String message, final Object response) {
-      runOnMainThread(new Runnable() {
-         @Override
-         public void run() {
-            reply.error(tag, message, response);
-         }
-      });
-   }
+    void replyError(final MethodChannel.Result reply, final String tag, final String message, final Object response) {
+        runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                reply.error(tag, message, response);
+            }
+        });
+    }
 
-   void replyNotImplemented(final MethodChannel.Result reply) {
-      runOnMainThread(new Runnable() {
-         @Override
-         public void run() {
-            reply.notImplemented();
-         }
-      });
-   }
+    void replyNotImplemented(final MethodChannel.Result reply) {
+        runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                reply.notImplemented();
+            }
+        });
+    }
 
-   private void runOnMainThread(final Runnable runnable) {
-      ((Activity) flutterRegistrar.activeContext()).runOnUiThread(runnable);
-   }
+    private void runOnMainThread(Runnable runnable) {
+        if (Looper.getMainLooper().getThread() == Thread.currentThread())
+            runnable.run();
+        else {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(runnable);
+        }
+    }
 
-   void invokeMethodOnUiThread(final String methodName, final HashMap map) {
-      final MethodChannel channel = this.channel;
-      runOnMainThread(new Runnable() {
-         @Override
-         public void run() {
-            channel.invokeMethod(methodName, map);
-         }
-      });
-   }
+    void invokeMethodOnUiThread(final String methodName, final HashMap map) {
+        final MethodChannel channel = this.channel;
+        runOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                channel.invokeMethod(methodName, map);
+            }
+        });
+    }
 }
