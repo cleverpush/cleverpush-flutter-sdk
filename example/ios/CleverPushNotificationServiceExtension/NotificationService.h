816aa4efc55862c2801ca675c85df3cf6de1e7d1
gbhrdt <marius.gebhardt@gmail.com>
2023-01-26 14:49:58 +0100

    Update example code

---
+++ example/ios/CleverPushNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  CleverPushNotificationServiceExtension
+//
+//  Created by Marius Gebhardt on 26/01/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
